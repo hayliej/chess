624752620ad8789845abf6fdebf6ca4b50a0523e
Haylie Jarvis <hjballet@yahoo.com>
2024-02-23 19:47:24 -0700

    server runs, but does nothing

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -6,13 +6,17 @@ public class Server {
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
-
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/login", this::login);
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private Object login(Request req, Response res) {
+        return "";
     }
 
     public void stop() {
