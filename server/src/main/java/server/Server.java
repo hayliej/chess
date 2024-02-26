2cde0c9d11c0f6c7bc8484774aebc77780224a7b
Haylie Jarvis <hjballet@yahoo.com>
2024-02-26 15:04:31 -0700

    added comments of what I think classes should do

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,8 +2,17 @@ package server;
 
 import passoffTests.testClasses.TestException;
 import spark.*;
+import service.Service;
+import dataAccess.DataAccessException;
 
 public class Server {
+    private final Service service;
+    //websocket??
+
+    public Server(DataAccessException dataAccess){
+        service = new Service(dataAccess);
+        //websocket??
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -22,9 +31,11 @@ public class Server {
         return Spark.port();
     }
 
+    //FOR ALL OF THE ABOVE, TAKE INPUT JSON CONVERT TO GSON, CALL SERVICE METHOD ON IT, RETURN TO CLIENT ???
+
     private Object clear(Request req, Response res) {
         //clear db
-        req.();
+        req.clear();
         return ""; //return cleared db
     }
 
--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -1,5 +1,25 @@
 package service;
 import server.Server;
+import dataAccess.DataAccessException;
 
 public class Service {
+    //what do we do with this??
+    private final DataAccessException dataAccess;
+
+    public Service(DataAccessException dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    //THINGS SERVICE NEEDS TO DO: ???
+    //getUser
+    //createUser
+    //createAuth
+    //checkValidUser
+    //getAuth
+    //get/listGames
+    //getGame
+    //createGame
+    //clearDB
+
+    //^NEED TO THROW EXCEPTIONS?
 }
