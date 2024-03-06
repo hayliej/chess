5f35e567af0407cb88ee785e8c5f5cefdb5503bf
Haylie Jarvis <hjballet@yahoo.com>
2024-03-06 14:40:30 -0700

    implemented adding into users and auth, probs dont work great yet

--- server/src/main/java/dataAccess/SqlAuthDAO.java
+++ server/src/main/java/dataAccess/SqlAuthDAO.java
@@ -21,7 +21,14 @@ public class SqlAuthDAO implements AuthDAO{
 
     @Override
     public void addAuth(AuthData authdata) throws DataAccessException {
-
+        var statement = "INSERT INTO auths (authToken, username) VALUES (?, ?)";
+        try (PreparedStatement state = DatabaseManager.getConnection().prepareStatement(statement)) {
+            state.setString(1, authdata.authToken());
+            state.setString(2, authdata.username());
+            state.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
