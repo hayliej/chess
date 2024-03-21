37238c00ac1888979836c3932cc25e5e209a9b54
Haylie Jarvis <hjballet@yahoo.com>
2024-03-21 13:16:47 -0600

    things are broken, SFTest outline

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -26,4 +26,87 @@ public class ServerFacadeTests {
         Assertions.assertTrue(true);
     }
 
+    //login positive
+    @Test
+    public void loginPositive() {
+
+        //Assertions.assertTrue(true);
+    }
+    //login negative
+    @Test
+    public void loginNegative() {
+
+        //Assertions.assertTrue(true);
+    }
+
+
+    //register positive
+    @Test
+    public void registerPositive() {
+
+        //Assertions.assertTrue(true);
+    }
+    //register negative
+    @Test
+    public void registerNegative() {
+
+        //Assertions.assertTrue(true);
+    }
+
+
+    //logout positive
+    @Test
+    public void logoutPositive() {
+
+        //Assertions.assertTrue(true);
+    }
+    //logout negative
+    @Test
+    public void logoutNegative() {
+
+        //Assertions.assertTrue(true);
+    }
+
+
+    //createGame positive
+    @Test
+    public void createGamePositive() {
+
+        //Assertions.assertTrue(true);
+    }
+    //createGame negative
+    @Test
+    public void createGameNegative() {
+
+        //Assertions.assertTrue(true);
+    }
+
+
+    //joinGame positive
+    @Test
+    public void joinGamePositive() {
+
+        //Assertions.assertTrue(true);
+    }
+    //joinGame negative
+    @Test
+    public void joinGameNegative() {
+
+        //Assertions.assertTrue(true);
+    }
+
+
+    //observeGame positive
+    @Test
+    public void observeGamePositive() {
+
+        //Assertions.assertTrue(true);
+    }
+    //observeGame negative
+    @Test
+    public void observeGameNegative() {
+
+        //Assertions.assertTrue(true);
+    }
+
 }
