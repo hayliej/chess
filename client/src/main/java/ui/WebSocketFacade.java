1a5d7e381b31fd5b7007a59d4b71607c2d114678
Haylie Jarvis <hjballet@yahoo.com>
2024-04-04 17:34:57 -0600

    start websocketFacade

--- client/src/main/java/ui/WebSocketFacade.java
+++ client/src/main/java/ui/WebSocketFacade.java
@@ -4,5 +4,34 @@ import java.net.URI;
 import java.util.Scanner;
 
 public class WebSocketFacade {
+    public static void main(String[] args) throws Exception {
+        var ws = new WebSocketFacade();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("echo");
+        while (true) ws.send(scanner.nextLine());
+    }
+
+    public Session session;
+
+    public WebSocketFacade() throws Exception {
+        URI uri = new URI("ws://localhost:8080/connect");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.session = container.connectToServer(this, uri);
+
+        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+            public void onMessage(String message) {
+                System.out.println(message);
+            }
+        });
+    }
+
+    public void send(String msg) throws Exception {
+        this.session.getBasicRemote().sendText(msg);
+    }
+
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+        //???
+    }
 
 }
