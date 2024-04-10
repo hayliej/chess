d3f60ef2c48ad97238840584d400f9df780a5b14
Haylie Jarvis <hjballet@yahoo.com>
2024-04-10 12:01:25 -0600

    working on gamerepl

--- client/src/main/java/ui/GameRepl.java
+++ client/src/main/java/ui/GameRepl.java
@@ -1,5 +1,9 @@
 package ui;
 
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import com.google.gson.Gson;
 import dataAccess.DataAccessException;
 
 
@@ -55,19 +59,52 @@ public class GameRepl {
     }
 
     //SEND THE MESSAGES FROM HERE. THIS IS WHERE THE TYPE COMES FROM.
-    private static void makeMove(String input) { //fix this later
+    private static void makeMove(String input) throws Exception { //fix this later
         //parse out input to get ID and color
-//        String[] in = input.split(" <");
-//        String g = in[1];
-//        String gID = g.replace(">", "");
-//        Integer gIDNum = Integer.valueOf(gID);
-//        String s = in[2];
-//        String start = s.replace(">", "");
-//        String e = in[2];
-//        String end = e.replace(">", "");
-//        String pt = in[2];
-//        String pieceType = pt.replace(">", "");
-//        ChessMove cm = new ChessMove(start, end, pieceType);
+        String[] in = input.split(" <");
+        for (String val : in){
+            if (val.length()>2){
+                continue;
+            }
+            val.replace("a", "1");
+            val.replace("b", "2");
+            val.replace("c", "3");
+            val.replace("d", "4");
+            val.replace("e", "5");
+            val.replace("f", "6");
+            val.replace("g", "7");
+            val.replace("h", "8");
+        }
+        String g = in[1];
+        String gID = g.replace(">", "");
+        Integer gIDNum = Integer.valueOf(gID);
+        String s = in[2];
+        String start = s.replace(">", "");
+        ChessPosition cps = new ChessPosition(s.charAt(0), s.charAt(1));
+        String e = in[2];
+        String end = e.replace(">", "");
+        ChessPosition cpe = new ChessPosition(e.charAt(0), e.charAt(1));
+        String pt = in[2];
+        String pieceType = pt.replace(">", "");
+        ChessPiece.PieceType ptype = ChessPiece.PieceType.PAWN;
+        switch (pieceType){
+            case "king":
+               ptype = ChessPiece.PieceType.KING;
+            case "knight":
+                ptype = ChessPiece.PieceType.KNIGHT;
+            case "queen":
+                ptype = ChessPiece.PieceType.QUEEN;
+            case "rook":
+                ptype = ChessPiece.PieceType.ROOK;
+            case "pawn":
+                ptype = ChessPiece.PieceType.PAWN;
+            case "bishop":
+                ptype = ChessPiece.PieceType.BISHOP;
+        }
+
+        ChessMove cm = new ChessMove(cps, cpe, ptype);
+        String msg = new Gson().toJson(cm);
+        WebSocketFacade.send(msg); //??
 //        UserGameCommand ugc = new MakeMove(authToken, gIDNum, cm);
 //        //need to make into gson for server?
 //        new WSServer(session, ugc);
